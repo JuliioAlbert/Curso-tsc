b3410407a998830fcc42fd9b5a60f1373d78206a
Julio Gonzalez <juliioalbert98@gmail.com>
2021-09-16 17:47:16 -0500

    Funciones:Required

---
+++ funciones/arg-required.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+    var fullName = function (fistName, lastName) {
+        return fistName + " " + lastName;
+    };
+    var name = fullName("Tony", "Stark");
+    console.log({ name: name });
+})();
