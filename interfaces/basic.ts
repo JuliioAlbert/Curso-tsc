461af335ebe112b9ee1746fa7d1cb789453909e5
Julio Gonzalez <juliioalbert98@gmail.com>
2021-09-21 23:43:48 -0500

    Interfaz Basica

---
+++ interfaces/basic.ts
@@ -0,0 +1,21 @@
+( () => {
+    type Hero = {
+        name:string, 
+        edad?:number, 
+        powers:number[], 
+        getName?:() => string
+    }
+
+
+    let myCustomVariable: string | number | Hero = 'Julio';
+    console.log(typeof myCustomVariable); 
+    myCustomVariable= 20;
+    console.log(typeof myCustomVariable); 
+    myCustomVariable= {
+        name: "Julio",
+        powers: [1],
+        edad: 21,
+        
+    };
+    console.log( myCustomVariable ); 
+})()
--- main.js
+++ main.js
@@ -1,91 +1,14 @@
 "use strict";
 (() => {
-    class Mutante {
-        constructor(name, realName) {
-            this.name = name;
-            this.realName = realName;
-        }
-    }
-    class Xmen extends Mutante {
-        salvarMundo() {
-            return `Salvar el mundo!`;
-        }
-    }
-    class Villian extends Mutante {
-        conquistarMundo() {
-            return `Mundo Conquistado!`;
-        }
-    }
-    const wolverine = new Xmen("Wolverine", "Logan");
-    const magento = new Villian("Magneto", "Mage");
-    const printName = (character) => {
+    let myCustomVariable = 'Julio';
+    console.log(typeof myCustomVariable);
+    myCustomVariable = 20;
+    console.log(typeof myCustomVariable);
+    myCustomVariable = {
+        name: "Julio",
+        powers: [1],
+        edad: 21,
     };
-    printName(wolverine);
-})();
-(() => {
-    class Aveger {
-        constructor(name, team, realName) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
-        }
-        static getAvgAge() {
-            return this.name;
-        }
-        bio() {
-            return `${this.name} (${this.team})`;
-        }
-    }
-    Aveger.age = 35;
-    const antMan = new Aveger('AntMan', 'Capitan', 'Scott Lang');
-})();
-(() => {
-    class Aveger {
-        constructor(name, realName) {
-            this.name = name;
-            this.realName = realName;
-        }
-        getFullName() {
-            return `${this.name}  ${this.realName}`;
-        }
-    }
-    class Xmen extends Aveger {
-        constructor(name, realName, isMutant) {
-            super(name, realName);
-            this.isMutant = isMutant;
-        }
-        get fullName() {
-            return `${this.name} - ${this.realName}`;
-        }
-        set fullName(name) {
-            this.name = name;
-        }
-        getFullNameXmen() {
-        }
-    }
-    const woll = new Xmen("Wolverine", 'Logan', true);
-    woll.fullName = "Julio";
-    woll.getFullNameXmen();
-})();
-(() => {
-    class Apocalipsis {
-        constructor(name) {
-            this.name = name;
-        }
-        static callApocalipsis() {
-            if (!Apocalipsis.instance) {
-                Apocalipsis.instance = new Apocalipsis("Soy Apocalipsis");
-            }
-            return Apocalipsis.instance;
-        }
-        changeName(newName) {
-            this.name = newName;
-        }
-    }
-    const apocalipsis = Apocalipsis.callApocalipsis();
-    const apocalipsis1 = Apocalipsis.callApocalipsis();
-    const apocalipsis2 = Apocalipsis.callApocalipsis();
-    apocalipsis1.changeName("Sas");
-    console.log(apocalipsis, apocalipsis1, apocalipsis2);
+    console.log(myCustomVariable);
 })();
 //# sourceMappingURL=main.js.map
