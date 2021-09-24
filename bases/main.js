acb239537e6411e85e2fcd9669043c6740420c53
Julio Gonzalez <juliioalbert98@gmail.com>
2021-09-23 22:37:11 -0500

    ts-node-clases-exports

---
+++ bases/main.js
@@ -0,0 +1,10 @@
+"use strict";
+var Validation;
+(function (Validation) {
+    Validation.validateText = (text) => (text.length > 3) ? true : false;
+    Validation.validateDate = (fecha) => {
+        return (isNaN(fecha.valueOf())) ? false : true;
+    };
+})(Validation || (Validation = {}));
+console.log(Validation.validateText("Juli"));
+//# sourceMappingURL=main.js.map
